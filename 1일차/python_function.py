4c5b561906816e31d6f4415ad9cbca240ae109ff
dahyunyang <manysheep24@gmail.com>
2022-10-25 12:58:22 +0900

    1일차 폴더 생성

---
+++ 1일차/python_function.py
@@ -0,0 +1,51 @@
+#def (define)
+# def name(input):
+#     print("제 이름은 " + input + "입니다." )
+
+# name("다현")
+
+# def sum(a,b):
+#     print(a + b)
+#     return a+b # return 은 output의 의미.
+
+# total = sum(3,4) # return이 없었다면 none
+# print(total)
+
+# Quiz
+
+# def check_grade(성적, 이름):
+#     if 성적 >= 90:
+#         print(이름 + "의 등급은 A입니다.")
+#     elif 성적 >= 80:
+#         print(이름 + "의 등급은 B입니다.")
+#     elif 성적 >= 70:
+#         print(이름 + "의 등급은 C입니다.")
+#     elif 성적 >= 60:
+#         print(이름 + "의 등급은 D입니다.")
+#     else:
+#         print(이름 + "의 등급은 F입니다.")
+
+#     return
+
+# check_grade(90, "다현")
+
+# grade = ""
+# def check_grade(성적, 이름):
+#     if 성적 >= 90:
+#         grade = 'A'
+#     elif 성적 >= 80:
+#         grade = 'B'
+
+#     return grade
+
+# a = check_grade(100)
+# print(a)
+
+# 파이썬 내장함수
+# a = "python"
+# a.upper()
+# "Python" =! "python"
+# print(a)
+
+# 클래스 : 안에 여러 개의 함수가 모여있다. 개념만 알아두기
+# 모듈 : 여러개의 클래스가 모여있는 것.
